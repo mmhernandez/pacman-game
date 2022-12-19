dd4a64fa4e6acabbe706afc56cb7cd2c7d0f68b3
mmhernandez <mmdangelo11@gmail.com>
2022-12-18 22:10:16 -0700

    styling tweaks

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 var worldGlossary = {
     "0": "empty",
-    "1": "wall",
+    1: "wall",
     "2": "coin",
     "3": "large-coin",
-    "4": "cherry",
-    "5": "pacman"
+    "4": "cherry"
 }
 var world1 = [
     [1,1,1,1,1,1,1,1,1,1],
-    [1,5,2,2,2,2,2,3,2,1],
+    [1,0,2,2,2,2,2,3,2,1],
     [1,1,1,1,1,1,1,1,2,1],
     [1,1,1,1,1,1,1,1,2,1],
     [1,2,2,2,2,1,1,1,2,1],
@@ -20,7 +19,7 @@ var world1 = [
 ]
 var world2 = [
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,5,2,2,2,2,2,3,2,1,2,2,2,2,1,1],
+    [1,0,2,2,2,2,2,3,2,1,2,2,2,2,1,1],
     [1,1,1,1,1,1,1,1,2,1,2,1,1,2,1,1],
     [1,1,1,1,1,1,1,1,2,1,2,1,1,3,1,1],
     [1,2,2,2,2,1,1,1,2,1,2,1,1,2,1,1],
@@ -32,7 +31,7 @@ var world2 = [
 ]
 var world3 = [
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,5,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1],
+    [1,0,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1],
     [1,2,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,2,2,1,1],
     [1,2,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,2,2,1,1],
     [1,2,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,2,2,1,1,1],
@@ -43,6 +42,8 @@ var world3 = [
     [1,1,2,2,2,1,1,1,2,2,2,1,1,1,1,1,1,2,2,2,2,2,2,2,1,1,1],
 	[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 ]
+
+// var level = document.getElementById("level");
 
 function displayWorld(level) {
     var worldToBuild;
@@ -58,18 +59,23 @@ function displayWorld(level) {
     for(var i=0; i<worldToBuild.length; i++) {
         output += `<div class="row">`;
         for(var j=0; j<worldToBuild[i].length; j++){
-            if(worldToBuild[i][j] == 5) {
-                output += `\t<div id="${worldGlossary[worldToBuild[i][j]]}"></div>`;
-            } else {
-                output += `\t<div class="${worldGlossary[worldToBuild[i][j]]}"></div>`;
-            }
+            output += `\t<div class="${worldGlossary[worldToBuild[i][j]]}"></div>`;
         }
         output += `</div>`;
     }
     document.querySelector(".world").innerHTML = output;
 }
-
 displayWorld(2);
+
+// var pacman = {
+//     x: 1,
+//     y: 1
+// }
+// function placePacman() {
+//     document.getElementById("pacman").style.left = `${pacman.x * 40}px`;
+//     document.getElementById("pacman").style.top = `${pacman.y * 40}px`;
+// }
+// placePacman();
 
 document.onkeydown = function(event) {
     //UP = 38
@@ -88,4 +94,6 @@ document.onkeydown = function(event) {
     if(event.keyCode == 37) {
         console.log("move left");
     }
+
+
 }
