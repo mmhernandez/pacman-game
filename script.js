ee2332adeafb0a8075fdb7ae22c8b6d1bdce0a61
mmhernandez <mmdangelo11@gmail.com>
2022-12-19 22:06:32 -0700

    work work work

--- script.js
+++ script.js
@@ -43,9 +43,9 @@ var world3 = [
 	[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 ]
 
-// var level = document.getElementById("level");
-function displayWorld(level) {
+function worldToDisplay() {
     var worldToBuild;
+    var level = document.getElementById("level").innerText;
     if (level == 1) {
         worldToBuild = world1;
     } else if(level == 2) {
@@ -53,48 +53,52 @@ function displayWorld(level) {
     }else if(level == 3) {
         worldToBuild = world3;
     }
+    return worldToBuild;
+}
 
+var world = worldToDisplay();
+function displayWorld() {
     var output = '';
-    for(var i=0; i<worldToBuild.length; i++) {
+    for(var i=0; i<world.length; i++) {
         output += `<div class="row">`;
-        for(var j=0; j<worldToBuild[i].length; j++){
-            output += `\t<div class="${worldGlossary[worldToBuild[i][j]]}"></div>`;
+        for(var j=0; j<world[i].length; j++){
+            output += `\t<div class="${worldGlossary[world[i][j]]}"></div>`;
         }
         output += `</div>`;
     }
     document.querySelector(".world").innerHTML = output;
+
+    // alert("Let's play Pacman!");
 }
-displayWorld(2);
+displayWorld();
 
 
 var pacmanPosition = {
-    x: 1,
-    y: 1
+    x: 2,
+    y: 2
 }
 function placePacman() {
-    console.log(document.getElementById("pacman"));
-    // document.getElementById("pacman").style.left = `${pacman.x * 20}px`; //pacmanPosition.x * 20 + "px";
-    // document.getElementById("pacman").style.top = `${pacman.y * 20}px`; //pacmanPosition.y * 20 + "px"; 
+    pacman.style.left = pacmanPosition.x * 20 + "px";
+    pacman.style.top = pacmanPosition.y * 20 + "px";
 }
 placePacman();
 
 document.onkeydown = function(event) {
-    //UP = 38
-    if(event.keyCode == 38) {
-        console.log("move up");
+    //MOVE UP = 38
+    if(event.keyCode == 38 && (world[pacmanPosition.y - 1][pacmanPosition.x] != 1)) {
+        pacmanPosition.y--;
     }
-    //RIGHT = 39
-    if(event.keyCode == 39) {
-        console.log("move right");
+    //MOVE RIGHT = 39
+    else if(event.keyCode == 39 && (world[pacmanPosition.y][pacmanPosition.x + 1] != 1)) {
+        pacmanPosition.x++;
     }
-    //DOWN = 40
-    if(event.keyCode == 40) {
-        console.log("move down");
+    //MOVE DOWN = 40
+    else if(event.keyCode == 40 && (world[pacmanPosition.y + 1][pacmanPosition.x]) != 1) {
+        pacmanPosition.y++;
     }
-    //LEFT = 37
-    if(event.keyCode == 37) {
-        console.log("move left");
+    //MOVE LEFT = 37
+    else if(event.keyCode == 37 && (world[pacmanPosition.y][pacmanPosition.x - 1] != 1)) {
+        pacmanPosition.x--;
     }
-
-
+    placePacman();
 }
