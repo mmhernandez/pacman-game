7e3bf804896a48beb932816cef6644dc35096ae8
mmhernandez <mmdangelo11@gmail.com>
2022-12-20 16:39:37 -0700

    finally got the pacman to do my bidding

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var world1 = [
     [1,1,1,1,1,1,1,1,2,1],
     [1,1,1,1,1,1,1,1,2,1],
     [1,2,2,2,2,1,1,1,2,1],
-    [1,2,1,1,2,1,1,1,2,1],
+    [1,2,1,1,2,1,1,1,4,1],
     [1,2,1,1,2,1,1,1,2,1],
     [1,3,1,1,2,1,1,1,1,1],
     [1,2,1,1,2,2,2,3,2,1],
@@ -19,27 +19,27 @@ var world1 = [
 ]
 var world2 = [
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,0,2,2,2,2,2,3,2,1,2,2,2,2,1,1],
+    [1,0,2,2,2,2,2,3,2,1,2,4,2,2,1,1],
     [1,2,1,1,1,1,1,1,2,1,2,1,1,2,1,1],
     [1,2,1,1,1,1,1,1,2,1,2,1,1,3,1,1],
     [1,2,2,2,2,1,1,1,2,1,2,1,1,2,1,1],
     [1,2,1,1,2,1,1,1,2,1,2,2,2,2,2,1],
-    [1,2,2,2,2,2,2,2,2,2,2,1,1,2,1,1],
+    [1,2,2,2,2,4,2,2,2,2,2,1,1,2,1,1],
     [1,3,1,1,2,1,1,1,1,1,1,1,1,2,1,1],
-    [1,2,1,1,2,2,2,3,2,2,2,2,2,2,1,1],
+    [1,2,1,1,2,2,2,3,2,2,2,2,2,4,1,1],
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 ]
 var world3 = [
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,0,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1],
-    [1,2,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,2,2,1,1],
+    [1,0,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,3,2,2,2,2,2,2,1,1,1],
+    [1,2,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,2,4,1,1],
     [1,2,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,2,2,1,1],
     [1,2,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,2,2,1,1,1],
-    [1,2,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,2,2,2,2,1,1,1,1],
+    [1,2,1,1,1,1,1,1,1,1,1,4,1,1,1,1,1,1,1,2,2,4,2,1,1,1,1],
     [1,2,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,2,2,1,1,1],
-    [1,2,1,1,1,2,2,2,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,2,2,1,1],
+    [1,2,1,1,1,2,3,2,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,2,2,1,1],
     [1,2,2,1,2,2,1,2,2,1,2,2,1,1,1,1,1,1,1,1,1,1,1,2,2,1,1],
-    [1,1,2,2,2,1,1,1,2,2,2,1,1,1,1,1,1,2,2,2,2,2,2,2,1,1,1],
+    [1,1,2,4,2,1,1,1,2,2,2,1,1,1,1,1,1,2,2,2,2,2,2,2,1,1,1],
 	[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 ]
 var world = worldToDisplay();
@@ -81,53 +81,55 @@ var pacmanPosition = {
 function placePacman() {
     pacman.style.top = pacmanPosition.y * 20 + "px";
     pacman.style.left = pacmanPosition.x * 20 + "px";
-
-    console.log(`after updating position: \ny:${pacmanPosition.y}  x:${pacmanPosition.x}`)
 }
 placePacman();
 
-document.onkeydown = function(event) {
+document.onkeydown = function(event) {    
     // the world array in the conditionals subtract 1 from each x/y coordinate b/c arrays start at position 0
-
-    if(event.key == "ArrowDown" && world[pacmanPosition.y][pacmanPosition.x-1] != 1) { 
-        console.log(`keypress: ${event.key}`);
-        console.log(`previous position type: ${worldGlossary[world[pacmanPosition.y-1][pacmanPosition.x-1]]}`);
-        //want to move down - must add 1 to outer array (y)
-        console.log(`new position should be: \ny:${pacmanPosition.y+1}  x:${pacmanPosition.x}`);
-        console.log(`new position type: ${worldGlossary[world[pacmanPosition.y][pacmanPosition.x-1]]}`);
-        
-        pacman.style.transform = `rotate(90deg)`;
-        pacmanPosition.y++;
-    }
-    else if(event.key == "ArrowRight" && world[pacmanPosition.y-1][pacmanPosition.x] != 1) {
-        console.log(`keypress: ${event.key}`);
-        console.log(`previous position type: ${worldGlossary[world[pacmanPosition.y-1][pacmanPosition.x-1]]}`);
+    if(event.key == "ArrowRight" && world[pacmanPosition.y-1][pacmanPosition.x] != 1) {
         //want to move right - must add 1 to inner array (x)
-        console.log(`new position should be: \ny:${pacmanPosition.y}  x:${pacmanPosition.x+1}`);
-        console.log(`new position type: ${worldGlossary[world[pacmanPosition.y][pacmanPosition.x+1]]}`);
-
-        pacman.style.transform = `rotate(0deg)`;
         pacmanPosition.x++;
+        pacman.style.transform = `rotate(0deg)`;
     }
-    else if(event.key == "ArrowUp" && world[pacmanPosition.y-2][pacmanPosition.x-1] != 1) {
-        console.log(`keypress: ${event.key}`);
-        console.log(`previous position type: ${worldGlossary[world[pacmanPosition.y-1][pacmanPosition.x-1]]}`);
-        //want to move up - must subtrack 1 from the outer array (y)
-        console.log(`new position should be: \ny:${pacmanPosition.y-1}   x:${pacmanPosition.x}`);
-        console.log(`new position type: ${worldGlossary[world[pacmanPosition.y][pacmanPosition.x+1]]}`);
-        
-        pacman.style.transform = `rotate(270deg)`;
-        pacmanPosition.y--;
+    else if(event.key == "ArrowDown" && world[pacmanPosition.y][pacmanPosition.x-1] != 1) {
+        //want to move down - must add 1 to outer array (y)
+        pacmanPosition.y++;
+        pacman.style.transform = `rotate(90deg)`;
     }
-    else if(event.key = "ArrowLeft" && world[pacmanPosition.y-1][pacmanPosition.x-2] != 1) {  
-        console.log(`keypress: ${event.key}`);
-        console.log(`previous position type: ${worldGlossary[world[pacmanPosition.y-1][pacmanPosition.x-1]]}`);
+    else if(event.key == "ArrowLeft" && world[pacmanPosition.y-1][pacmanPosition.x-2] != 1) {
         //want to move left - must subtract 1 from the inner array (x)
-        console.log(`new position should be: \ny:${pacmanPosition.y}   x:${pacmanPosition.x-1}`);
-        console.log(`new position type: ${worldGlossary[world[pacmanPosition.y][pacmanPosition.x+1]]}`);
-        
-        pacman.style.transform = `rotate(180deg)`;
         pacmanPosition.x--;
+        pacman.style.transform = `rotate(180deg)`;
     }
+    else if(event.key == "ArrowUp"  && world[pacmanPosition.y-2][pacmanPosition.x-1] != 1) {
+        //want to move up - must subtrack 1 from the outer array (y)
+        pacmanPosition.y--;
+        pacman.style.transform = `rotate(270deg)`;
+    }
+    updateScore(updateWorld());
     placePacman();
+}
+
+function updateWorld() {
+    // console.log(`current position: ${world[pacmanPosition.y+1][pacmanPosition.x+1]}`)
+    // console.log(`position to update: ${worldGlossary[world[pacmanPosition.y-1][pacmanPosition.x-1]]}`)
+    // if(world[pacmanPosition.y+1][pacmanPosition.x+1] == 2) {
+    //     world[pacmanPosition.y+1][pacmanPosition.x+1] = 0;
+    //     return 2;
+    // }
+    return 0;
+}
+
+var currentScore = score.innerText;
+function updateScore(item) {
+    if(item == 2) {
+        currentScore++;
+    }
+    else if(item == 3) {
+        currentScore += 2;
+    }
+    else if(item == 4) {
+        currentScore += 50;
+    }
+    score.innerText = currentScore;
 }
